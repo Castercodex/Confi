a2662bc431dd02d91067eb5af3c221faba55cdae
Castercodex <edwardberth1@gmail.com>
2020-12-26 02:20:36 +0100

    confi

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,17 @@ let nav = document.querySelector(".nav-links");
 let cancel = document.querySelector(".cancel");
 let navShow = document.querySelector(".nav-show");
 let navCircle = document.querySelector(".nav-circle");
+let navBar = document.querySelector("nav");
+
+if (window.outerWidth > 768) {
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 70) {
+      navBar.classList.add("nav");
+    } else {
+      navBar.classList.remove("nav");
+    }
+  });
+}
 
 burger.onclick = () => {
   nav.classList.toggle("nav-mobile");
@@ -11,5 +22,5 @@ burger.onclick = () => {
 };
 
 navCircle.onclick = () => {
-    navCircle.classList.toggle("active");
+  navCircle.classList.toggle("active");
 };
